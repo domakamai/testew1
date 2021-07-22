91e7dfcdb93a4474724cf77a8ed48ab85130183e
Dominic Lovell <dlovell@syd-mpo2s.lan>
2021-07-22 13:53:57 +1000

    Remove ex

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 export function onClientRequest(request) {
     request.respondWith(
         200, {},
-        '<html><body><h1>Hello World From Akamai EdgeWorkers!</h1></body></html>');
+        '<html><body><h1>Hello World From Akamai EdgeWorkers</h1></body></html>');
   }
   
   export function onClientResponse(request, response) {
