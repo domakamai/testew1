d4f4ade979f3e80f5b1e3813702cd92a8cc6fbd0
Dominic Lovell <dlovell@syd-mpo2s.lan>
2021-07-22 12:15:44 +1000

    add ew

---
+++ main.js
@@ -0,0 +1,9 @@
+export function onClientRequest(request) {
+    request.respondWith(
+        200, {},
+        '<html><body><h1>Hello World From Akamai EdgeWorkers</h1></body></html>');
+  }
+  
+  export function onClientResponse(request, response) {
+    response.setHeader('X-Hello-World', 'From Akamai EdgeWorkers');
+  }
